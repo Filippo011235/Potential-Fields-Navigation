678dc397c8e30ee3d8d0c14f7c699c38bf32ed4a
Filippo011235 <fadamcewicz1@gmail.com>
2019-10-21 16:33:55 +0200

    Podpunkt A, poczatek B

--- lab01/lab01_MetPolPotencjalnych.py
+++ lab01/lab01_MetPolPotencjalnych.py
@@ -5,16 +5,41 @@ import matplotlib.cbook as cbook
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
 
+def RandomCoordinate():
+    """ Return random coordinate for an object (float). """ 
+    return np.random.uniform(-10,11)
 
-delta = 0.01
+def ForcesInPoint():
+
+def AttractivePotential():
+    k_p = 1
+    
+
+def RepulsivePotentialFromObstacle():
+
+##### DELTA ZWIEKSZONA ######
+delta = 0.1
+#############################
 x_size = 10
 y_size = 10
-obst_vect = [(1, 2), (1.5, 2.3), (0,1), (1,0)]
-start_point=(-10,-3)
-finish_point=(10,3)
+
+# Defining Start and Finish points, and Obstacles
+start_point=(-10,RandomCoordinate())
+finish_point=(10,RandomCoordinate())
+
+### OBSTACLES
+obst_vect = []
+NoOfObstacles = 4
+for i in range(0,NoOfObstacles):
+    obst_vect.append((RandomCoordinate(), RandomCoordinate()))
+# Coefficients:
+k_0 = 1
+d_0 = 20
+
 
 x = y = np.arange(-10.0, 10.0, delta)
 X, Y = np.meshgrid(x, y)
+# Matrix Z of forces affecting the robot in given point
 Z = np.exp(-X**0)
 
 
